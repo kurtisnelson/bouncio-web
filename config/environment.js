20499734851a745523620cae2fa7ad342e150703
Kurt Nelson <kurtisnelson@gmail.com>
2015-09-03 11:25:47 -0500

    Setup CSP

--- config/environment.js
+++ config/environment.js
@@ -13,6 +13,17 @@ module.exports = function(environment) {
       }
     },
 
+    contentSecurityPolicyHeader: 'Content-Security-Policy',
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self'",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    },
+
     APP: {
       apiHost: 'https://bouncer-core.herokuapp.com'
     }
