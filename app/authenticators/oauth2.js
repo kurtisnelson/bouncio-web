793c9aff5f86817dd98388c886d009d23e80afd3
Kurt Nelson <kurtisnelson@gmail.com>
2015-10-03 20:49:48 -0500

    ember-simple-auth changed again

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -3,5 +3,6 @@ import ENV from '../config/environment';
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-    serverTokenEndpoint: ENV.APP.apiHost + '/session'
+    serverTokenEndpoint: ENV.APP.apiHost + '/session',
+    serverTokenRevocationEndpoint: ENV.APP.apiHost + '/session'
 });
--- config/environment.js
+++ config/environment.js
@@ -26,12 +26,11 @@ module.exports = function(environment) {
 
     APP: {
       apiHost: 'https://bouncio-core.herokuapp.com'
-    },
-    'ember-simple-auth': {
-      base: {
-        store: 'session-store:local-storage'
-      }
     }
+  };
+
+  ENV['ember-simple-auth'] = {
+    store: 'session-store:local-storage'
   };
 
   if (environment === 'development') {
